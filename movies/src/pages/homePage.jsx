8b4c37cd2e4b41b0667abf545cfca4cdf310c68e
JakeDsetu <20101923@mail.wit.ie>
2025-02-12 10:05:20 +0000

    Filter card and Header components added

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -9,7 +9,7 @@ const HomePage = (props) => {
 
   useEffect(() => {
     fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
     )
       .then((res) => res.json())
       .then((json) => {
